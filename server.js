5fcd98ecce957be01f2efc00d7ecba2fa7231227
Amir Hamza <amirhamja4bd@gmail.com>
2023-06-07 16:30:29 +0600

    Update server.js

--- server.js
+++ server.js
@@ -13,10 +13,12 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cors = require('cors');
 
-// Database
+//DataBase 
 const mongoose = require('mongoose');
+// app.use(express.static('client/dist'));
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
 
-// Middleware configuration
+// Security Middleware implementation
 app.use(cors());
 app.use(helmet());
 app.use(mongoSanitize());
@@ -27,17 +29,13 @@ app.use(hpp());
 app.use(bodyParser.json());
 
 // Rate Limit
-const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 3000 });
-app.use(limiter);
+const limiter = rateLimit({ windowMs: 15 * 60 * 60, max: 3000 });
 
-// Serve static files
-app.use(express.static(path.join(__dirname, 'client', 'dist')));
-
-// API routes
+// Manage BackEnd Routings
 app.use('/api/v1', router);
 
-// Frontend route
-app.get('*', (req, res) => {
+// Manage Frontend Routes
+app.get('*', function (req, res) {
   res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
 });
 
@@ -48,82 +46,12 @@ const port = process.env.PORT || 5000;
 // Connect to Database and start server
 mongoose.set('strictQuery', true);
 mongoose
-  .connect(database, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(database, { autoIndex: true })
   .then(() => {
-    // Check if running in AWS Lambda environment
-    if (process.env.LAMBDA_TASK_ROOT && process.env.AWS_EXECUTION_ENV) {
-      const serverlessExpress = require('aws-serverless-express');
-      const server = serverlessExpress.createServer(app);
-      module.exports.handler = (event, context) => {
-        serverlessExpress.proxy(server, event, context);
-      };
-    } else {
-      // Start server locally
-      app.listen(port, () => {
-        console.log(`Server running on port ${port}`);
-      });
-    }
+    app.listen(port, () => {
+      console.log(`Server Running on port ${port}`);
+    });
   })
   .catch((error) => console.log(error));
 
-
-
-
-
-// const express = require('express');
-// const router = require('./src/routes/api');
-// const app = express();
-// const bodyParser = require('body-parser');
-// const path = require('path');
-// require('dotenv').config();
-
-// // Secure Middleware
-// const rateLimit = require('express-rate-limit');
-// const helmet = require('helmet');
-// const mongoSanitize = require('express-mongo-sanitize');
-// const xss = require('xss-clean');
-// const hpp = require('hpp');
-// const cors = require('cors');
-
-// //DataBase 
-// const mongoose = require('mongoose');
-// // app.use(express.static('client/dist'));
-// app.use(express.static(path.join(__dirname, 'client', 'dist')));
-
-// // Security Middleware implementation
-// app.use(cors());
-// app.use(helmet());
-// app.use(mongoSanitize());
-// app.use(xss());
-// app.use(hpp());
-
-// // Body Parser
-// app.use(bodyParser.json());
-
-// // Rate Limit
-// const limiter = rateLimit({ windowMs: 15 * 60 * 60, max: 3000 });
-
-// // Manage BackEnd Routings
-// app.use('/api/v1', router);
-
-// // Manage Frontend Routes
-// app.get('*', function (req, res) {
-//   res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
-// });
-
-// // Database Connection
-// const database = process.env.DATABASE_URL;
-// const port = process.env.PORT || 5000;
-
-// // Connect to Database and start server
-// mongoose.set('strictQuery', true);
-// mongoose
-//   .connect(database, { autoIndex: true })
-//   .then(() => {
-//     app.listen(port, () => {
-//       console.log(`Server Running on port ${port}`);
-//     });
-//   })
-//   .catch((error) => console.log(error));
-
-// module.exports = app;
+module.exports = app;
