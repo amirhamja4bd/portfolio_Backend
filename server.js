22228a1a86f7b5f8391c8fe06301f3c67c909c06
Amir Hamza <amirhamja4bd@gmail.com>
2023-06-07 23:12:05 +0600

    Update server.js

--- server.js
+++ server.js
@@ -35,6 +35,19 @@ const limiter = rateLimit({ windowMs: 15 * 60 * 60, max: 3000 });
 // Manage BackEnd Routings
 app.use('/api/v1', router);
 
+
+app.get('/', (req, res)=>{
+    res.status(200).send(`
+        <div class="container mt-5">
+            <div class="jumbotron text-center">
+                <h1 class="fs-3">Welcome to My E-Commerce Server</h1>
+                <p class="lead">Visit My Website:- 
+                <a class="" href="https://ceommerce-client.vercel.app/"> My E-Commerce Website</a></p>
+            </div>
+        </div>
+    `);
+});
+
 // Manage Frontend Routes
 app.get('*', function (req, res) {
   res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
