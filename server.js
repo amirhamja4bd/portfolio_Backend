914ca69eba7bc7d53f37383e6aa1a44f85bbd580
Amir Hamza <amirhamja4bd@gmail.com>
2023-06-07 22:39:27 +0600

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
 require('dotenv').config();
-
 // Secure Middleware
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
@@ -12,9 +11,11 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cors = require('cors');
-
 //DataBase 
 const mongoose = require('mongoose');
+// app.use(express.static('client/dist'));
+// app.use(express.static(path.join(__dirname, 'client', 'dist')));
+app.use(express.static(path.join(__dirname, 'client')));
 
 // Security Middleware implementation
 app.use(cors());
@@ -22,31 +23,19 @@ app.use(helmet());
 app.use(mongoSanitize());
 app.use(xss());
 app.use(hpp());
-
 // Body Parser
 app.use(bodyParser.json());
-
 // Rate Limit
 const limiter = rateLimit({ windowMs: 15 * 60 * 60, max: 3000 });
-
-
-// Manage BackEnd Routings
-app.use('/api/v1', router);
-
-// app.use(express.static('client/dist'));
-app.use(express.static(path.join(__dirname, 'client', 'dist')));
 // Manage Frontend Routes
 app.get('*', function (req, res) {
-  res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
 });
-
-
-
-
+// Manage BackEnd Routings
+app.use('/api/v1', router);
 // Database Connection
 const database = process.env.DATABASE_URL;
 const port = process.env.PORT || 5000;
-
 // Connect to Database and start server
 mongoose.set('strictQuery', true);
 mongoose
@@ -57,5 +46,69 @@ mongoose
     });
   })
   .catch((error) => console.log(error));
-
 module.exports = app;
+
+
+
+
+// const express = require('express');
+// const router = require('./src/routes/api');
+// const app = express();
+// const bodyParser = require('body-parser');
+// const path = require('path');
+// require('dotenv').config();
+
+// // Secure Middleware
+// const rateLimit = require('express-rate-limit');
+// const helmet = require('helmet');
+// const mongoSanitize = require('express-mongo-sanitize');
+// const xss = require('xss-clean');
+// const hpp = require('hpp');
+// const cors = require('cors');
+
+// //DataBase 
+// const mongoose = require('mongoose');
+
+// // Security Middleware implementation
+// app.use(cors());
+// app.use(helmet());
+// app.use(mongoSanitize());
+// app.use(xss());
+// app.use(hpp());
+
+// // Body Parser
+// app.use(bodyParser.json());
+
+// // Rate Limit
+// const limiter = rateLimit({ windowMs: 15 * 60 * 60, max: 3000 });
+
+
+// // Manage BackEnd Routings
+// app.use('/api/v1', router);
+
+// // app.use(express.static('client/dist'));
+// app.use(express.static(path.join(__dirname, 'client', 'dist')));
+// // Manage Frontend Routes
+// app.get('*', function (req, res) {
+//   res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+// });
+
+
+
+
+// // Database Connection
+// const database = process.env.DATABASE_URL;
+// const port = process.env.PORT || 5000;
+
+// // Connect to Database and start server
+// mongoose.set('strictQuery', true);
+// mongoose
+//   .connect(database, { autoIndex: true })
+//   .then(() => {
+//     app.listen(port, () => {
+//       console.log(`Server Running on port ${port}`);
+//     });
+//   })
+//   .catch((error) => console.log(error));
+
+// module.exports = app;
