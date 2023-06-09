7f940b8e41c7874a5e1ac9cd56f55833e5c14563
Amir Hamza <amirhamja4bd@gmail.com>
2023-06-10 00:20:52 +0600

    Update server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.get('/example', (req, res) => {
   
   // Serve index.html for all other routes
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'index.html'));
   });
 
 // Database Connection
