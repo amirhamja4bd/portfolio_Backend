7c7c26f3a0fa56e5f2abada64a64b35194882385
Amir Hamza <amirhamja4bd@gmail.com>
2023-06-10 00:30:05 +0600

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  
   plugins: [react()],
+  build: {
+    // ... other build options
+
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/main.jsx'),
+      },
+    },
+  },
 })
+
